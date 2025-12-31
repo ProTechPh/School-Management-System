09068f07713be35e1244608b03679285c1e4b4d4
[dyad] <jerickogarcia0@gmail.com>
2025-12-31 23:27:51 +0800

    [dyad] Removing setup page - wrote 1 file(s), deleted 1 file(s)

--- app/setup/page.tsx
+++ app/setup/page.tsx
@@ -1,8 +1,5 @@
+import { notFound } from "next/navigation"
+
 export default function SetupPage() {
-  return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">Setup</h1>
-      <p>System setup page.</p>
-    </div>
-  )
+  notFound()
 }
