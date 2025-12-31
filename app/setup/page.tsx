19296f031f37a5df3100fd4b27122b648fe36044
[dyad] <jerickogarcia0@gmail.com>
2025-12-31 23:26:42 +0800

    [dyad] Fixing missing setup page - wrote 1 file(s)

---
+++ app/setup/page.tsx
@@ -0,0 +1,8 @@
+export default function SetupPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Setup</h1>
+      <p>System setup page.</p>
+    </div>
+  )
+}
